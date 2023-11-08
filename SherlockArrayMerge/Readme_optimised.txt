c4662c3989dc756295aa68065940d98ba5ebfe59
Reakefit <mouni221100@gmail.com>
2023-11-08 22:41:11 +0530

    problem 1 - optimised

---
+++ SherlockArrayMerge/Readme_optimised.txt
@@ -0,0 +1,12 @@
+So I'm assuming it fails for large input sizes.
+
+One optimisation I can think of is using dicts to condense ones that have first 3 cols similar and just add the ways
+Because the output number won't need to differentiate between:
+1   2
+3
+and
+1
+3   2
+Both are same to it.
+
+And on submission we get all except one case :D let's work on it once done with the rest!
---
+++ SherlockArrayMerge/SherlockArray_optimised.py
@@ -0,0 +1,52 @@
+from collections import deque
+import time
+MOD = 10**9 + 7
+
+def arrayMerging(m):
+    mod = 10**9 + 7
+    q = deque()
+    q.append((m[0], 1, -1, 1))
+
+    for x in m[1:]:
+        nextQ = deque()
+        while q:
+            subseqMax, subseqLen, prevSubseqL, ways = q.popleft()
+
+            if x >= subseqMax:
+                if prevSubseqL > subseqLen:
+                    nextQ.append((x, subseqLen + 1, prevSubseqL, ways * (prevSubseqL - subseqLen) % mod))
+                if prevSubseqL == -1:
+                    nextQ.append((x, subseqLen + 1, -1, 1))
+            nextQ.append((x, 1, subseqLen, (subseqLen * ways) % mod))
+
+        condensed_nextQ = deque()
+        condensed = {}
+        for item in nextQ:
+            key = item[:3]
+            if key in condensed:
+                condensed[key] = (condensed[key] + item[3]) % mod
+            else:
+                condensed[key] = item[3]
+
+        for key, combined_ways in condensed.items():
+            condensed_nextQ.append((*key, combined_ways))
+        q = condensed_nextQ
+        print(q)
+    return sum(ways for _, _, _, ways in q) % mod
+
+
+if __name__ == '__main__':
+    m = [i for i in range(1, 101)]
+    
+    startTime = time.time()
+    print(arrayMerging(m))
+    print(time.time() - startTime)
+
+    m = [1, 3, 2, 4]
+    m3 = [1, 2, 3]
+    m2 = [2, 1]
+    m4 = [1,2,5,3,4]
+    print(arrayMerging(m))
+    print(arrayMerging(m3))
+    print(arrayMerging(m4))
+    print(arrayMerging(m2))
